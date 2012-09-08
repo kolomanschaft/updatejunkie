1efa705d2f4017b39fb7a31bb609a4e9574c4731
koloman <gestatten@gmail.com>
2012-09-08 13:37:58 +0200

    did some commenting and removed the observer.log

--- main.py
+++ main.py
@@ -5,14 +5,29 @@ from WillhabenObserver import *
 from Logger import *
 import md5
 import sys
+import os
 
 if __name__ == "__main__":
 	
+	# Ads will be observed from this URL
 	url = "http://www.willhaben.at/iad/kaufen-und-verkaufen/marktplatz?ISPRIVATE=1&CATEGORY/SUBCATEGORY=8621&CATEGORY/MAINCATEGORY=73"
-
-	save_file = "files/" + md5.new(url).hexdigest() + ".save"
+	
+	# Create the 'files/' directory if it doesn't exist yet
+	if not os.path.exists("./files/"): os.mkdir("files")
+	
+	# Initialize logging
+	logger = Logger(path = "files/observer.log")
+	logger.append("Setting up observer")
+	
+	# ----------------------------------------------
+	# Set up of the platform independent components
+	# ----------------------------------------------
+	
+	# Ads that have already been processed are registered in this file
+	save_file = "files/" + md5.new(url).hexdigest() + ".save"	
 	store = AdStore(path = save_file)
 	
+	# Set up your search criteria here!
 	kwdsAll = AdCriterionTitleKeywordsAll(["galaxy"])
 	kwdsAny = AdCriterionTitleKeywordsAny(["s3", "sIII", "i9300"])
 	kwdsNot = AdCriterionTitleKeywordsNot(["freischalten"])
@@ -21,8 +36,9 @@ if __name__ == "__main__":
 	assessor = AdAssessor()
 	assessor.add_criteria(kwdsAll, kwdsAny, kwdsNot, price)
 	
-	logger = Logger(path = "files/observer.log")
-	logger.append("Setting up observer")
+	# ---------------------------------------------------------
+	# Set up of the platform dependent components and run loop
+	# ---------------------------------------------------------
 	
 	if sys.platform == "linux2":
 		from platform_dependant.Linux import GTKNotification
