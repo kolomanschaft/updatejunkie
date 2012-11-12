fe1706cc125330a8a9b23e55fff725691289ccdc
Martin Hammerschmied <gestatten@gmail.com>
2012-11-12 16:07:08 +0100

    The new connector profiles allow for polling arbitrary content-based websites. Using regular expressions ads are extracted from the website and then processed by the Connector. WillhabenConnector was renamed to Connector.

--- Connector.py
+++ Connector.py
@@ -6,68 +6,125 @@ WillhabenConnector.py
 Created by Martin Hammerschmied on 2012-09-09.
 Copyright (c) 2012. All rights reserved.
 """
-import httplib
-from bs4 import BeautifulSoup as bs
+import urllib2
+from urllib import urlencode
 import re
 from AdStore import Ad
+from Profile import *
+import datetime
 
-class WillhabenConnectionError(Exception): pass
+class ConnectionError(Exception): pass
 
-class WillhabenConnector():
+class Connector():
     
-    def __init__(self, url):
-        m = re.match("(http:\/\/)?([a-zA-Z0-9-.]+)?([a-zA-Z0-9-._/?=&%]*)", url)
+    @property
+    def profile_name(self):
+        return self._profile.name
+    
+    def __init__(self, url, profile):
+        m = re.match(r"(http://)?([a-zA-Z0-9-.]+)?([a-zA-Z0-9-._/?=&%]*)", url)
         if m is None:
             raise Exception("Invalid URL: {}".format(url))
-        self.host = "www.willhaben.at"
-        self.url = m.groups()[2]
+        self._url = url
+        if isinstance(profile, Profile):
+            self._profile = profile
+        elif isinstance(profile, unicode) or isinstance(profile, str):
+            self._profile = get_profile(profile)
 
     def __get_page__(self, page):
-        page_match = re.search("page=[0-9]+", self.url)
-        if page_match:
-            self.url = self.url.replace(page_match.group(), "page="+str(page))
-        elif self.url.find("?") >= 0:
-            self.url = self.url + "&page="+str(page)
+        page_param = self._profile.page_param
+        new_encoded = urlencode({page_param["name"]: page})
+        data = None
+        if page_param["method"] == u"GET":
+            page_match = re.search(u"{}=[0-9]+".format(page_param["name"]), self._url)
+            if page_match:
+                self._url = self._url.replace(page_match.group(), new_encoded)
+            elif self._url.find(u"?") >= 0:
+                self._url = self._url + u"&" + new_encoded
+            else:
+                self._url = self._url + u"?" + new_encoded
         else:
-            self.url = self.url + "?page="+str(page)
-        willhaben = httplib.HTTPConnection(self.host)
-        try:
-            willhaben.request("GET", self.url)
-            response = willhaben.getresponse()
-        except:
-            raise WillhabenConnectionError()
-        html = response.read()
-        return unicode(html, "latin1")
+            data = new_encoded
+        
+        f = urllib2.urlopen(self._url, data)
+        html = unicode(f.read(), "ISO-8859-1")
+        f.close()
+        for regex in self._profile.format_regexes:
+            if regex["type"] == u"filter":
+                html = re.sub(regex["regex"], "", html)
+            else:
+                raise ConnectionError("Format type {} is not implemented!".format(regex["type"]))
+        return html
+
 
     def __get_adlist_from_html__(self, html):
-        doc = bs(html)
+        def text_tag(value):
+            return value
+        def integer_tag(value):
+            return int(value)
+        def float_tag(value):
+            return float(value.replace(",", "."))
+        def url_tag(value):
+            if value[0] == u"/":
+                return self._profile.base_url + value[1:]
+            else:
+                return value
+            
+        ad_tuples = re.findall(self._profile.ad_regex, html)
+        tags = self._profile.ad_tags
+        time_tag = self._profile.time_tag
+        
         ads = []
-        for li in doc.find_all("li"):
-            try:
-                if "clearfix" in li["class"]:
-                    new_ad = Ad(aid = int(li.h2["id"]),
-                                title = li.h2.a.contents[0],
-                                url = "http://" + self.host + li.h2.a["href"])
-                    for content in li.p.contents:
-                        m = re.search("[,0-9]+", content.string)
-                        if m:
-                            new_ad["price"] = float(m.group().replace(",", "."))
-                    ads.append(new_ad)
-            except KeyError:
-                continue
+        for ad_tuple in ad_tuples:
+            ad = Ad()
+            for combined in zip(tags, ad_tuple):
+                if combined[0]["type"] == u"text":
+                    ad[combined[0]["name"]] = text_tag(combined[1])
+                elif combined[0]["type"] == u"integer":
+                    ad[combined[0]["name"]] = integer_tag(combined[1])
+                elif combined[0]["type"] == u"float":
+                    ad[combined[0]["name"]] = float_tag(combined[1])
+                elif combined[0]["type"] == u"url":
+                    ad[combined[0]["name"]] = url_tag(combined[1])
+            ad.keytag = self._profile.key_tag
+            timetag_info = self._profile.time_tag
+            ad.timetag = datetime.datetime.strptime(ad[timetag_info["name"]], timetag_info["format"])
+            ads.append(ad)
         return ads
+            
     
     def frontpage_ads(self):
-        html = self.__get_page__(1)
-        ads = self.__get_adlist_from_html__(html)
-        return ads
+        html = self.__get_page__(self._profile.page_param["init"])
+        return self.__get_adlist_from_html__(html)
     
-    def all_ads(self, pagestart = 1, maxpages = 100):
-        all_ads = []
-        for page in range(pagestart,pagestart+maxpages):
+    def ads_all(self, pagestart = None, maxpages = 100):
+        timelimit = datetime.datetime(1970,1,1)
+        return self.ads_after(timelimit, maxpages)
+    
+    def ads_in(self, dtime, maxpages = 100):
+        if not isinstance(dtime, datetime.timedelta):
+            raise ConnectionError("timelimit needs to be a timedelta instance")
+        timelimit = datetime.datetime.now() - dtime
+        return self.ads_after(timelimit, maxpages)
+    
+    def ads_after(self, timelimit, maxpages = 100):
+        pagestart = self._profile.page_param["init"]
+        if not isinstance(timelimit, datetime.datetime):
+            raise ConnectionError("timelimit needs to be a datetime instance")
+        ads = []
+        for page in xrange(pagestart,pagestart+maxpages):
             html = self.__get_page__(page)
-            ads = self.__get_adlist_from_html__(html)
-            if len(ads) == 0:
+            new_ads = [ad for ad in self.__get_adlist_from_html__(html) if ad.timetag > timelimit]
+            if len(new_ads) == 0:
                 break
-            all_ads.extend(ads)
-        return all_ads
+            ads.extend(new_ads)
+        return ads
+
+if __name__ == "__main__":
+    url = u"http://www.willhaben.at/iad/kaufen-und-verkaufen/moebel-wohnen-buero/regale-schraenke-vitrinen-kommoden/"
+    c = Connector(url, "willhaben")
+    #html = c.__get_page__(1)
+    #print c.__get_adlist_from_html__(html)
+    #print [ad.key for ad in c.__get_adlist_from_html__(html)]
+    print [ad.timetag for ad in c.ads_in(datetime.timedelta(hours = 2))]
+    
--- Profile.py
+++ Profile.py
@@ -9,9 +9,18 @@ Copyright (c) 2012. All rights reserved.
 
 from xml.dom.minidom import parse
 import os
-import re
 
 class ProfileError(Exception):pass
+
+def get_profile(name):
+    folder = "connector_profiles/"
+    filenames = os.listdir(folder)
+    for filename in filenames:
+        if filename[-3:] == u"xml":
+            p = Profile(folder+filename)
+            if p.name == name:
+                return p
+    raise ProfileError("No connector profile with the name of {} found".format(name))
 
 class Profile(object):
     
@@ -19,7 +28,8 @@ class Profile(object):
         if not os.path.exists(path):
             raise ProfileError("File {} does not exist!".format(path))
         dom = parse(path)
-        self._website = self._childNode(u"website", dom)
+        self._profile = self._childNode(u"profile", dom)
+        self._website = self._childNode(u"website", self._profile)
     
     def _childNodes(self, tag, node = None):
         if not node:
@@ -36,12 +46,19 @@ class Profile(object):
         tags = self._childNodes(u"#text", node)
         return tags[0]
     
+    @property
+    def name(self):
+        nameNode = self._childNode(u"name", self._profile)
+        return self._firstTextChild(nameNode).nodeValue
+    
+    @property
     def base_url(self):
         baseUrlNode = self._childNode(u"baseUrl")
         if not baseUrlNode.hasChildNodes:
             raise ProfileError("Profile baseUrl is empty!")
         return self._firstTextChild(baseUrlNode).nodeValue
     
+    @property
     def ad_regex(self):
         adDefinitionNode = self._childNode(u"adDefinition")
         regexNode = self._childNode(u"regex", adDefinitionNode)
@@ -49,28 +66,49 @@ class Profile(object):
             raise ProfileError("Profile adDefinition lacks a regex!")
         return self._firstTextChild(regexNode).nodeValue
     
+    @property
     def ad_tags(self):
         adDefinitionNode = self._childNode(u"adDefinition")
         tagNodes = self._childNodes(u"tag", adDefinitionNode)
         tags = [{"name": self._firstTextChild(tagNode).nodeValue, "type": tagNode.getAttribute(u"type")} for tagNode in tagNodes]
         return tags
     
+    @property
+    def time_tag(self):
+        adDefinitionNode = self._childNode(u"adDefinition")
+        timeTagNode = self._childNode(u"timeTag", adDefinitionNode)
+        tagNameNode = self._childNode(u"tagName", timeTagNode)
+        formatNode = self._childNode(u"format", timeTagNode)
+        return {"name": self._firstTextChild(tagNameNode).nodeValue, "format": self._firstTextChild(formatNode).nodeValue}
+    
+    @property
+    def key_tag(self):
+        adDefinitionNode = self._childNode(u"adDefinition")
+        keyTagNode = self._childNode(u"keyTag", adDefinitionNode)
+        tagNameNode = self._childNode(u"tagName", keyTagNode)
+        return self._firstTextChild(tagNameNode).nodeValue
+       
+    
+    @property
     def format_regexes(self):
         def make_dict(regexNode):
             return {"regex": self._firstTextChild(regexNode).nodeValue, "type": regexNode.getAttribute(u"type")}
         formatNode = self._childNode(u"format")
         return [make_dict(regexNode) for regexNode in self._childNodes(u"regex", formatNode)]
     
+    @property
     def page_param(self):
         pageParamNode = self._childNode(u"pageParam")
         return {"name": self._firstTextChild(pageParamNode).nodeValue, "method": pageParamNode.getAttribute(u"method"), "init": int(pageParamNode.getAttribute(u"init"))} 
 
 if __name__ == '__main__':
-    path = u"connector_profiles/willhaben.xml"
-    profile = Profile(path)
+    profile = get_profile(u"willhaben")
+    print "Profile Name:", profile.name()
     print "baseUrl", profile.base_url()
     print "adRegex", profile.ad_regex()
     print "tags", profile.ad_tags()
+    print "timeTag", profile.time_tag()
+    print "keyTag", profile.key_tag()
     print "format", profile.format_regexes()
     print "pageParam", profile.page_param()
     
