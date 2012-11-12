9f1d428d19a4d01e455ec3950a4f3c932f61583b
Martin Hammerschmied <gestatten@gmail.com>
2012-11-12 16:12:06 +0100

    The new profile tester supports the creation of profiles

---
+++ profile_tester.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+profile_tester.py
+
+Created by Martin Hammerschmied on 2012-11-12.
+Copyright (c) 2012. All rights reserved.
+"""
+import sys
+from Profile import *
+from Connector import *
+from AdStore import *
+import pprint
+
+if __name__ == "__main__":
+    try:
+        profile = unicode(sys.argv[1])
+        url = unicode(sys.argv[2])
+    except IndexError:
+        print "Arguments error!"
+        print "Usage: python profile_tester.py profile_name url"
+        exit()
+    pp = pprint.PrettyPrinter()
+    print "\n\nTesting Profile '{}'".format(profile)
+    print "with URL", url
+    
+    def print_ads(ads):
+        for ad in ads:
+            try:
+                keytag = ad.key
+                print "\nAd-ID:", keytag
+            except AdKeyError:
+                print "\nNo keytag set! Set ad.keytag"
+            print "Tags:"
+            pp.pprint(ad)
+            try:
+                timetag = ad.timetag
+                print "TimeTag:", timetag
+            except AdKeyError:
+                print "No timetag!"
+
+    c = Connector(url, profile)
+    ads = c.frontpage_ads()
+    print_ads(ads)
+
+    print "\n{} ads found on the first page".format(len(ads))
