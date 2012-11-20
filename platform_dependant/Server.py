f6a2ed362a7f1e2d88f6dc9a8579c1b2f9955ab2
Martin Hammerschmied <gestatten@gmail.com>
2012-11-20 15:45:46 +0100

    Fixed the from-header in the email notifications

--- platform_dependant/Server.py
+++ platform_dependant/Server.py
@@ -19,18 +19,19 @@ class EmailNotification(Notification):
 		self.sender = sender
 		self.to = to
 		self.subject = subject
-		MIMEHeader = smtplib.email.mime.Text.MIMEText("", mimetype, "utf-8")
-		MIMEHeader["From"] = self.sender.encode("utf-8")
-		#MIMEHeader["To"] = self.to.encode("utf-8")
-		MIMEHeader["Subject"] = self.subject.encode("utf-8")
-		self.msg = unicode(MIMEHeader.as_string(), "utf-8") + body
+		self.body = body
+		self.headers = smtplib.email.mime.Text.MIMEText("", mimetype, "utf-8")
+		self.headers["From"] = self.sender.encode("utf-8")
+		self.headers["Subject"] = self.subject.encode("utf-8")
 
 	def notify(self, ad):
 		server = smtplib.SMTP(self.host, self.port)
 		server.login(self.user, self.pw)
 		for to in self.to:
 			try:
-				server.sendmail(self.sender, to, self.msg.format(**ad).encode("utf-8"))
+				self.headers["To"] = to.encode("utf-8")
+				msg = unicode(self.headers.as_string(), "utf-8") + self.body
+				server.sendmail(self.sender, to, msg.format(**ad).encode("utf-8"))
 			except KeyError as expn:
 				raise NotificationError("Profile doesn't support tagname '{}'".format(expn.message))
 
